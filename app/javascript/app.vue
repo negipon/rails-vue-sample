9770c8c7c03fde870938202839e90eb7534b526b
根岸周一 <s.negishi@negishishuuichinoMacBook-ea.local>
2018-10-06 11:03:23 +0900

    up

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p class="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  font-size: 10vw;
+  text-align: center;
+}
+</style>
